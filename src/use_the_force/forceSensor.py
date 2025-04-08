6b23bdcd84331b941bccef75c0727b32db16781c
Pieter van Toorenenburg <p.h.van.toorenenburg@vu.nl>
2025-04-08 09:25:48 +0200

    moved commands inside forceSensor class (non gui version)

--- src/use_the_force/forceSensor.py
+++ src/use_the_force/forceSensor.py
@@ -1,5 +1,5 @@
-from time import perf_counter_ns
-
+from time import perf_counter_ns, sleep
+import serial
 class ForceSensor():
     def __init__(self, PortName: str = "/dev/ttyACM0", GaugeValue: int = 0, NewtonPerVolt: float = 0.0000154, WarningOn: bool = True, MaxNewton: int | float = 5, **kwargs) -> None:
         """
@@ -8,8 +8,6 @@ class ForceSensor():
         (PySerial library has to be installed on the computer, see requirements.txt)
         """
         ####### SOME PARAMETERS AND STUFF ######
-        
-        import serial
 
         self.GaugeValue: int = GaugeValue  # The 'zero' volt value. Determined automatically each time.
         self.NewtonPerVolt: float = NewtonPerVolt
@@ -19,8 +17,13 @@ class ForceSensor():
 
         self.encoding: str = kwargs.pop('encoding', "UTF-8")
 
-        self.baudrate: int = kwargs.pop('baudrate', 57600)
+        self.baudrate: int = kwargs.pop('baudrate', 115200)
         self.timeout: float = kwargs.pop('timeout', 2)
+
+        self.stdDelay: float = 0 #150/1000 # base delay of the M5Din Meter to send a response [seconds]
+        self.cmdStart: str = "#"
+        self.cmdEnd: str = ";"
+        self.currentReads = [[], []]
 
         self.T0 = perf_counter_ns()
 
@@ -31,30 +34,31 @@ class ForceSensor():
         # To find the correct port: go to Windows Settings, Search for Device Manager,
         # and click the tab "Ports (COM&LPT)".
 
-        self.ser = serial.Serial(self.PortName,
-                            baudrate=self.baudrate,
-                            timeout=self.timeout)
+        self.ser = self.connectPort(self.PortName, baudrate=self.baudrate, timeout=self.timeout)
 
         # Test whether we are receiving any data or not.
-        try: 
-            line = self.ser.readline()
-            decodedLine = line.decode(self.encoding)
-            if decodedLine == "":
-                raise RuntimeError("Loadsensor returns no data")
+        # try: 
+        #     line = self.ser.readline()
+        #     decodedLine = line.decode(self.encoding)
+        #     if decodedLine == "":
+        #         raise RuntimeError("Loadsensor returns no data")
 
-            if GaugeValue == 0:
-                print("Communication with the LoadSensor established")
-                self.reGauge()
-            else:
-                print("Communication with the LoadSensor established")
-                print("Given gauged value: " + str(self.GaugeValue))
+        #     if GaugeValue == 0:
+        #         print("Communication with the LoadSensor established")
+        #         self.reGauge()
+        #     else:
+        #         print("Communication with the LoadSensor established")
+        #         print("Given gauged value: " + str(self.GaugeValue))
         
-        except UnicodeDecodeError:
-            print("could not decode incoming data!")
-            print("Connection maintained for debugging.")
-            print("Data: ", line)
-            print("Decoded: "+ str(line.decode(self.encoding, errors="replace")))
+        # except UnicodeDecodeError:
+        #     print("could not decode incoming data!")
+        #     print("Connection maintained for debugging.")
+        #     print("Data: ", line)
+        #     print("Decoded: "+ str(line.decode(self.encoding, errors="replace")))
     
+    def connectPort(self, PortName: str, baudrate: int = 115200, timeout: int = 2):
+        return serial.Serial(PortName, baudrate=baudrate, timeout=timeout)
+
 
     def reGauge(self):
         """
@@ -123,3 +127,302 @@ class ForceSensor():
             print(line, " --> ", decodedLine)
             print("Force: " + str(float(decodedLine.split(",")[1])) + " N")
 
+    def SP(self, position: int) -> None:
+        """
+        ### Set Position
+        Sets the position of the steppermotor stage in milimeters.
+        
+        :param position: position to set from bottom [mm]
+        :type position: int
+        """
+        self.ser.flush()
+        self.ser.flushInput()
+        self.ser.flushOutput()
+        self.ser.write(f"{self.cmdStart}SP {position}{self.cmdEnd}".encode())
+        if self.stdDelay > 0:
+            sleep(self.stdDelay)
+        returnLine = self.ser.read_until().decode().strip()
+        if returnLine.split(":")[0] == "[ERROR]":
+            raise RuntimeError(returnLine)
+    
+    def GP(self) -> int:
+        """
+        ### Get Position
+        Current position set in memory.
+        
+        :return: End position if moving, else current position in [mm]
+        :rtype: int
+        """
+        self.ser.flush()
+        self.ser.flushInput()
+        self.ser.flushOutput()
+        self.ser.write(f"{self.cmdStart}GP{self.cmdEnd}".encode())
+        if self.stdDelay > 0:
+            sleep(self.stdDelay)
+        returnLine = self.ser.read_until().decode().strip()
+        if returnLine.split(":")[0] == "[ERROR]":
+            raise RuntimeError(returnLine)
+        else:
+            try:
+                return int(returnLine.split(": ")[-1])
+            except ValueError as e:
+                return e
+    
+    def SV(self, velocity: int) -> str:
+        """
+        ### Set Velocity
+        Sets the velocity of the steppermotor stage in milimeters per second.
+        
+        :param velocity: velocity to set [mm/s]
+        :type velocity: int
+        """
+        self.ser.flush()
+        self.ser.flushInput()
+        self.ser.flushOutput()
+        self.ser.write(f"{self.cmdStart}SV {velocity}{self.cmdEnd}".encode())
+        if self.stdDelay > 0:
+            sleep(self.stdDelay)
+        returnLine = self.ser.read_until().decode().strip()
+        if returnLine.split(":")[0] == "[ERROR]":
+            raise RuntimeError(returnLine)
+
+    def GV(self) -> int:
+        """
+        ### Get Velocity
+        Returns the current velocity of the steppermotor stage in milimeters per second.
+        
+        :return: End velocity if moving, else current velocity [mm/s]
+        :rtype: int
+        """
+        self.ser.flush()
+        self.ser.flushInput()
+        self.ser.flushOutput()
+        self.ser.write(f"{self.cmdStart}GV{self.cmdEnd}".encode())
+        if self.stdDelay > 0:
+            sleep(self.stdDelay)
+        returnLine = self.ser.read_until().decode().strip()
+        if returnLine.split(":")[0] == "[ERROR]":
+            raise RuntimeError(returnLine)
+        else:
+            try:
+                return int(returnLine.split(": ")[-1])
+            except ValueError as e:
+                return e
+            
+    def GM(self) -> str:
+        """
+        ### Get Mode
+        Returns the current selected mode, calibrated or raw.
+
+        :return: string with selected mode
+        :rtype: str
+        """
+        self.ser.flush()
+        self.ser.flushInput()
+        self.ser.flushOutput()
+        self.ser.write(f"{self.cmdStart}GV{self.cmdEnd}".encode())
+        if self.stdDelay > 0:
+            sleep(self.stdDelay)
+        returnLine = self.ser.read_until().decode().strip()
+        if returnLine.split(":")[0] == "[ERROR]":
+            raise RuntimeError(returnLine)
+        else:
+            try:
+                return returnLine.split(" ")[-1]
+            except ValueError as e:
+                return e
+        
+
+    def TM(self) -> None:
+        """
+        ### Toggle Mode
+        Toggles between the two modes, calibrated or raw.
+        """
+        self.ser.flush()
+        self.ser.flushInput()
+        self.ser.flushOutput()
+        self.ser.write(f"{self.cmdStart}TM{self.cmdEnd}".encode())
+        if self.stdDelay > 0:
+            sleep(self.stdDelay)
+        returnLine = self.ser.read_until().decode().strip()
+        if returnLine.split(":")[0] == "[ERROR]":
+            raise RuntimeError(returnLine)
+        
+    def SR(self) -> float:
+        """
+        ### Single Read
+        Reads the force a single time.
+
+        :return: read force
+        :rtype: float
+        """
+        self.ser.flush()
+        self.ser.flushInput()
+        self.ser.flushOutput()
+        self.ser.write(f"{self.cmdStart}SR{self.cmdEnd}".encode())
+        if self.stdDelay > 0:
+            sleep(self.stdDelay)
+        returnLine = self.ser.read_until().decode().strip()
+        if returnLine.split(":")[0] == "[ERROR]":
+            raise RuntimeError(returnLine)
+        else:
+            try:
+                return float(returnLine.split(": ")[-1])
+            except ValueError as e:
+                return e
+
+    def CR(self, nReads: int, iReads: int) -> list[list]:
+        """
+        ### Continuous Reading
+        Reads nReads times the force with an iReads interval inbetween.
+        
+        :param nReads: number of lines to read
+        :type nReads: int
+        :param iReads: interval inbetween lines [ms]
+        :type iReads: int
+
+        :return: [[time], [force]]
+        :rtype: list[list[int], list[float]]
+        """
+        self.ser.flush()
+        self.ser.flushInput()
+        self.ser.flushOutput()
+        self.ser.write(f"{self.cmdStart}CR {nReads},{iReads}{self.cmdEnd}".encode())
+        sleep(self.stdDelay + iReads/1000)
+        self.ser.read_until("[TIME;VALUE]:")
+        returnLine = self.ser.read_until().decode().strip()
+        if returnLine.split(":")[0] == "[ERROR]":
+            raise RuntimeError(returnLine)
+        else:
+            time, force = returnLine.split(": ")[-1].split(";")
+            time = int(time)
+            force = float(force)
+            del self.currentReads
+            self.currentReads = [[time], [force]]
+
+            for i in range(nReads):
+                returnLine = self.ser.read_until().decode().strip()
+                if returnLine.split(":")[0] == "[ERROR]":
+                    raise RuntimeError(returnLine)
+                else:
+                    time, force = returnLine.split(": ")[-1].split(",")
+                    time = int(time)
+                    force = float(force)
+                    self.currentReads[0].append(time)
+                    self.currentReads[1].append(force)
+            return self.currentReads
+
+    def HM(self) -> None:
+        """
+        ### Home
+        Homes the steppermotor stage to the endstop.
+        The endstop is a physical switch that stops the motor when it is pressed.
+        Afterwards goes up to a set position inside the firmware.
+        """
+        self.ser.flush()
+        self.ser.flushInput()
+        self.ser.flushOutput()
+        self.ser.write(f"{self.cmdStart}HM{self.cmdEnd}".encode())
+        if self.stdDelay > 0:
+            sleep(self.stdDelay)
+        returnLine = self.ser.read_until().decode().strip()
+        if returnLine.split(":")[0] == "[ERROR]":
+            raise RuntimeError(returnLine)
+
+    def TR(self) -> None:
+        """
+        ### Tare
+
+        Tares load cell values by setting current reading as offset.
+        """
+        self.ser.flush()
+        self.ser.flushInput()
+        self.ser.flushOutput()
+        self.ser.write(f"{self.cmdStart}TR{self.cmdEnd}".encode())
+        if self.stdDelay > 0:
+            sleep(self.stdDelay)
+        returnLine = self.ser.read_until().decode().strip()
+        if returnLine.split(":")[0] == "[ERROR]":
+            raise RuntimeError(returnLine)
+
+    def CL(self) -> None:
+        """
+        ### Calibrate Loading
+
+        Starts the callibration sequence by calling TR and throws away the set slope value.
+
+        ## Make sure no force is applied to the sensor when calling this function!
+        """
+        self.ser.flush()
+        self.ser.flushInput()
+        self.ser.flushOutput()
+        self.ser.write(f"{self.cmdStart}TM{self.cmdEnd}".encode())
+        if self.stdDelay > 0:
+            sleep(self.stdDelay)
+        returnLine = self.ser.read_until().decode().strip()
+        if returnLine.split(":")[0] == "[ERROR]":
+            raise RuntimeError(returnLine)
+
+    def SF(self, calibrationForce: float) -> None:
+        """
+        ### Set Force
+
+        Only to be called when the sensor is in calibration mode.
+        Since this is scaling force, units can be set in [mN] or any other prefix as well. 
+        The ouput force will correspond with the unit set.
+
+        Internally the force is stored as a float, so it is recommended to use a unit corresponding to the measurement range as to avoid floating point errors.
+
+        :param calibrationForce: current force on the loadcell [N]
+        :type calibrationForce: float
+        """
+        self.ser.flush()
+        self.ser.flushInput()
+        self.ser.flushOutput()
+        self.ser.write(f"{self.cmdStart}SF {calibrationForce}{self.cmdEnd}".encode())
+        if self.stdDelay > 0:
+            sleep(self.stdDelay)
+        returnLine = self.ser.read_until().decode().strip()
+        if returnLine.split(":")[0] == "[ERROR]":
+            raise RuntimeError(returnLine)
+        
+
+    def SC(self) -> None:
+        """
+        ### Save Configuration
+
+        Saves the current calibration settings to flash memory and uses them on boot.
+        """
+        self.ser.flush()
+        self.ser.flushInput()
+        self.ser.flushOutput()
+        self.ser.write(f"{self.cmdStart}SC{self.cmdEnd}".encode())
+        if self.stdDelay > 0:
+            sleep(self.stdDelay)
+        returnLine = self.ser.read_until().decode().strip()
+        if returnLine.split(":")[0] == "[ERROR]":
+            raise RuntimeError(returnLine)
+    
+
+    def CC(self, iReads: int) -> list[list[int], list[float]]:
+        """
+        # DEBUG: only available in debug firmware.
+
+        ### CR, but as a toggle.
+        Will contiuously read until the command is sent again.
+        
+        :param iReads: interval inbetween reads [ms]
+        :type iReads: int
+
+        :return: [[time], [force]]
+        :rtype: list[list[int], list[float]]
+        """
+        self.ser.flush()
+        self.ser.flushInput()
+        self.ser.flushOutput()
+        self.ser.write(f"{self.cmdStart}CC {iReads}{self.cmdEnd}".encode())
+        if self.stdDelay > 0:
+            sleep(self.stdDelay)
+        returnLine = self.ser.read_until().decode().strip()
+        if returnLine.split(":")[0] == "[ERROR]":
+            raise RuntimeError(returnLine)
