ca0b88470b81cb08a52da65ed490d46a49e0427a
DylanvanArneman <51400178+DylanvanArneman@users.noreply.github.com>
2026-09-02 16:56:38 +0200

    First basic firmware for load cell + HX711
    Created a .ino file using the github.com/olkal HX711_ADC library. Right now, four basic commands are available: "*IDN?", "TARE", "CALIBRATE", and "MEASURE"

---
+++ thorlabs/visa_firmware/HX711_Force_Measurement_olkal_library.i.ino
@@ -0,0 +1,83 @@
+#include <HX711_ADC.h> // olkal library
+
+// Define a set of commands:
+#define COM_IDN        "*IDN?"       // Gives the identification number
+#define COM_TARE       "TARE"        // Command to tare the load cell
+#define COM_CALIBRATE  "CALIBRATE "  // Command to calibrate the load cell
+#define COM_MEASURE    "MEASURE?"    // Command to measure and read out from load cell
+
+// Arduino pins:
+const int HX711_dout = 4; // Arduino pin #4
+const int HX711_sck = 5;  // Arduino pin #5
+
+//HX711 constructor:
+HX711_ADC LoadCell(HX711_dout, HX711_sck); // Creating an object called LoadCell using these specific Arduino pins
+
+// Identification string:
+const char IDN_STRING[] = "Arduino HX711 Force Sensor v0.1.0";
+
+void setup() {
+  // This runs only once as soon as you start up
+  Serial.begin(57600); // The argument here is the baud rate, i.e. the speed at which the adruino and computer communicate over serial
+  Serial.setTimeout(100); // Prevents teh Arduino from getting stuck indefinitely if there is something wrong with the serial communication
+
+
+  LoadCell.begin();
+  unsigned long stabilizingtime = 2000; // precision right after power-up can be improved by adding a few seconds of stabilizing time (from olkal example) -> 2000 corresponds to a 2 second stabilization period
+  bool _tare = true; //set this to false if you don't want tare to be performed in the next step
+
+  LoadCell.start(stabilizingtime, _tare); // Actually starts the process
+
+}
+
+void loop() {
+  // Put your main code here, to run repeatedly:
+
+  LoadCell.update(); // The update() function checks for new data and starts the next conversion
+
+  if (Serial.available()) { // Here you can type someting into the serial monitor in order to give a command
+    
+    String command = Serial.readStringUntil('\n');
+    command.trim();
+
+    if (command == COM_IDN){
+      Serial.println(IDN_STRING);
+    }
+    
+    else if (command == COM_TARE) {
+      // Tare the load cell
+      LoadCell.tare(); // This comes from the olkal library
+      Serial.println("TARE COMPLETE");
+    }
+
+    else if (command.startsWith(COM_CALIBRATE)) {
+      // Calibrate the load cell
+      String valueString = command.substring(strlen(COM_CALIBRATE));
+      float known_mass = valueString.toFloat();
+
+      LoadCell.refreshDataSet();
+      float newCalibrationValue = LoadCell.getNewCalibration(known_mass);
+      LoadCell.setCalFactor(newCalibrationValue);
+
+      // Serial.print("CALIBRATION FACTOR: ");
+      // Serial.println(newCalibrationValue);
+      Serial.println("CALIBRATION COMPLETE");
+      }
+
+    else if (command == COM_MEASURE) {
+      // Measure a load
+      float value = LoadCell.getData();
+      Serial.println(value); // This prints the measured value onto the serial monitor
+    }
+
+    // Unknown command
+    else {
+
+      Serial.print("ERROR: UNKNOWN COMMAND ");
+      Serial.println(command);
+    }
+
+  }
+
+
+}
